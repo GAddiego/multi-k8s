4dbba409a50bf560d5827accf4cd42f4dead513a
GAddiego <gonzaloaddiego@gmail.com>
2024-11-07 14:15:41 +0100

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React or die!
+            Learn React or die!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
